9b784cbe5558f7edede6de37b9263b00ec26dffd
mjeanelwood <mjeanelwood@gmail.com>
2022-11-17 18:30:20 -0800

    Added moment.js for Los Angeles

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,14 @@
+function updateLosAngelesTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+setInterval(updateLosAngelesTime, 1);
