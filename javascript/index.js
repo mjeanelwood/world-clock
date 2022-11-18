e7ed7e417a545eb3fc3787b54ffc5f8fac6805a8
mjeanelwood <mjeanelwood@gmail.com>
2022-11-17 21:38:55 -0800

    Added current. location to select drop down

--- javascript/index.js
+++ javascript/index.js
@@ -41,6 +41,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -54,6 +57,7 @@ function updateCity(event) {
     "A"
   )}</small></div>
   </div>
+ 
   `;
 }
 
